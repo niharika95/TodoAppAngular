fcf64e87144d9d1c196ded884273a46e4b3fc7ce
Niharika <niharika13dalal@gmail.com>
2018-05-13 12:14:53 -0400

    toso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import {TodoDataService} from './todo-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app works!';
+
+  constructor(private todoDataService: TodoDataService){
+
+  }
+
+  toggleTodoComplete(todo){
+  	this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo){
+  	this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos(){
+  	return this.todoDataService.getAllTodos();
+  }
 }
